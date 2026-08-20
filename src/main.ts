c10ecd4c57f54f98bd03cc28b0e5d86152555d67
Rudra Nadpara <rudranadpara84@gmail.com>
2026-08-20 22:21:45 +0530

    Updated main.ts to bind to 0.0.0.0 for Render

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,11 @@ async function bootstrap() {
   // NEW: Increase payload limit to 10MB to handle image uploads
   app.use(json({ limit: '10mb' }));
 
-  await app.listen(3000);
-  console.log(`Backend API is running on: http://localhost:3000`);
+  //wait app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  
+  console.log(`Application is running on port: ${port}`);
+  //console.log(`Backend API is running on: http://localhost:3000`);
 }
 bootstrap();
